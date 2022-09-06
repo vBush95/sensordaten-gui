473f0dd7d5583b83c3744f43038f700552da7ee8
vBusch95 <janole.wiebusch@t-online.de>
2022-09-06 12:01:44 +0200

    added exampleEntitiesObject

---
+++ src/utils/exampleEntitiesObject.ts
@@ -0,0 +1,113 @@
+type Messwert = {
+  name: string;
+  device_class: string;
+  state_class: string;
+  unit_of_measurement: string;
+  range: number[];
+};
+
+type Sensor = {
+  type: string;
+  messwerte: Messwert[];
+} | null;
+
+type Group = string | null;
+
+type entitiesConstructor = {
+  numberOfDevices: number;
+  groups: Group[];
+  sensors: Sensor[][];
+};
+
+const exampleEntitiesObject: entitiesConstructor = {
+  numberOfDevices: 50,
+  groups: ["obergeschoss", "untergeschoss", "Z", "C", null],
+  sensors: [
+    [
+      {
+        type: "dht22",
+        messwerte: [
+          {
+            name: "temperatur",
+            device_class: "temperature",
+            state_class: "measurement",
+            unit_of_measurement: "°C",
+            range: [5, 35],
+          },
+          {
+            name: "luftfeuchtigkeit",
+            device_class: "humidity",
+            state_class: "measurement",
+            unit_of_measurement: "%",
+            range: [0, 100],
+          },
+        ],
+      },
+      {
+        type: "bme280",
+        messwerte: [
+          {
+            name: "temperatur",
+            device_class: "temperature",
+            state_class: "measurement",
+            unit_of_measurement: "°C",
+            range: [5, 35],
+          },
+          {
+            name: "luftfeuchtigkeit",
+            device_class: "humidity",
+            state_class: "measurement",
+            unit_of_measurement: "%",
+            range: [0, 100],
+          },
+          {
+            name: "luftdruck",
+            device_class: "pressure",
+            state_class: "measurement",
+            unit_of_measurement: "hPa",
+            range: [900, 1100],
+          },
+        ],
+      },
+    ],
+    [
+      {
+        type: "sds011",
+        messwerte: [
+          {
+            name: "pm10",
+            device_class: "dust",
+            state_class: "measurement",
+            unit_of_measurement: "µg/m³",
+            range: [0, 75],
+          },
+          {
+            name: "pm25",
+            device_class: "dust",
+            state_class: "measurement",
+            unit_of_measurement: "µg/m³",
+            range: [0, 75],
+          },
+        ],
+      },
+      null,
+    ],
+    [
+      {
+        type: "mhz19",
+        messwerte: [
+          {
+            name: "co2",
+            device_class: "air",
+            state_class: "measurement",
+            unit_of_measurement: "ppm",
+            range: [400, 2000],
+          },
+        ],
+      },
+      null,
+    ],
+  ],
+};
+
+export default exampleEntitiesObject;
