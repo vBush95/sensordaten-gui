584ac57f802e94048fcbadd831f19a838411ec23
vBusch95 <janole.wiebusch@t-online.de>
2022-09-16 11:05:11 +0200

    changed url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sensordaten-gui/sensordaten",
+  base: "/sensordaten-gui/",
   plugins: [react()],
   test: {
     coverage: {
