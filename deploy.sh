42e17d797cf8112ced351e1e6888b6a29daac8b9
vBusch95 <janole.wiebusch@t-online.de>
2022-09-16 09:48:28 +0200

    config changes

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,27 @@
 # abort on errors
 set -e
 
+# just to get us all at the same starting point
+git checkout demo-site
+
+# delete your build folder
+rm -rf dist/
+
+# create a 'build' directory checked out to the gh-pages branch
+git worktree add -B gh-pages dist origin/gh-pages
+
 # build
 npm run build
 
 # navigate into the build output directory
 cd dist
 
-git init
-git checkout -b demo-site
-git add -A
-git commit -m 'deploy'
+# fail if for some reason this isn't the gh-pages branch
+current_branch=$(git symbolic-ref --short -q HEAD)
+if [ "$current_branch" != "gh-pages" ]; then
+  echo "Expected build folder to be on gh-pages branch."
+  exit 1
+fi
 
-git push -u origin demo-site
+git add . && git commit -m "Update gh-pages"
+git push
